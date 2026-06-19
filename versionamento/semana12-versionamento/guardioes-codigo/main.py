8c604f639975fd473dd1d9d30a77fb7e42d2ac9c
luigi <00001106820939sp@al.educacao.sp.gov.br>
2026-06-19 17:59:33 -0300

    semana 9

---
+++ versionamento/semana12-versionamento/guardioes-codigo/main.py
@@ -0,0 +1,12 @@
+def somar(a, b):
+
+    return a + b
+
+
+def dividir(a, b):
+
+    return a / b
+
+def somar(a, b):
+
+    return a + b
---
+++ versionamento/semana12-versionamento/guardioes-codigo/test_main.py
@@ -0,0 +1,10 @@
+from main import somar, dividir
+
+def test_somar():
+
+   assert somar(2, 3) == 6 
+
+
+def test_dividir():
+
+    assert dividir(10, 2) == 5
